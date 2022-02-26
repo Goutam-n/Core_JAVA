658781940aebfd8242488288f8fd2909c501a408
Goutam <niwalgoutam@gmail.com>
2022-02-26 14:45:32 +0530

    Added esssential comments

--- src/Constructorss.java
+++ src/Constructorss.java
@@ -1,16 +1,18 @@
 package com.company;
-
+// JAVA program to illustrate constructor overloading by using same task for differrent types of argument
 class Construct
 {
-    String name;
+    // Constructor with one argument
     Construct(String name)
     {
         System.out.println("Constructor with one "+ "argument - string : "+ name);
     }
+    // Constructor with two argument
     Construct(String name, int age)
     {
         System.out.println("Constructor with two argument "+ "string and integer : "+ name+ " "+ age );
     }
+    // Constructor with one argument ( BUT WITH DIFFERENT TYPE THAN PREVIOUS )
     Construct(long id)
     {
         System.out.println("Constructor with one argument "+ "long : "+ id);
