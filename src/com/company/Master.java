0d10d9ab03e015efea410428fb0ec6262b6c5cc1
Goutam <niwalgoutam@gmail.com>
2022-02-26 15:10:47 +0530

    Edited one comment

--- src/com/company/Master.java
+++ src/com/company/Master.java
@@ -3,7 +3,7 @@ package com.company;
 public class Master {
     public static void main(String[] args) {
 
-        //invokes default constructo first
+        //invokes default constructor first
         new com.company.Constructor_chaining();
 
     }
