3d57021c79bf4f9a4392e1916d3afe6b784ae465
Goutam <niwalgoutam@gmail.com>
2022-02-27 21:35:55 +0530

    Practicing array programs

---
+++ src/Arrays.java
@@ -0,0 +1,13 @@
+package com.company;
+
+class Student
+{
+    int roll_no;
+    String name;
+
+    Student(int roll_no,String name)
+    {
+        this.roll_no = roll_no;
+        this.name = name;
+    }
+}
--- src/com/company/Master.java
+++ src/com/company/Master.java
@@ -5,10 +5,17 @@ import com.company.Derived;
 public class Master {
     public static void main(String[] args) {
 
-        //calls paremerizedconstructor 4
-        Derived obj= new Derived("Test");
-        // Calls No-argument constructor
-        // Derived obj = new Derived();
+        com.company.Student[] arr = new com.company.Student[5];
+        arr[0] = new com.company.Student(1,"abhi");
+        arr[1] = new com.company.Student(2,"sufi");;
+        arr[2] = new com.company.Student(3,"mufi");;
+        arr[3] = new com.company.Student(4,"suhi");;
+        arr[4] = new com.company.Student(5,"petie");;
+
+        for(int i=0;i< arr.length;i++)
+        {
+            System.out.println("Element at : "+ i + " is : "+ arr[i].roll_no + "," + arr[i].name);
+        }
 
     }
 }
